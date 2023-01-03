06f585e9ca0538baf5a1c456e8d42715fe9ceb83
Aléxia Vitória <aleeevitoria@gmail.com>
2023-01-03 16:41:34 -0300

    adjust the argument type

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import routeDictionary from './routeDictionary';
 
 
 export default{
-    async getZipCode(zipCode: number){
+    async getZipCode(zipCode: string){
         return await axios.get(`
                 https://viacep.com.br/${routeDictionary.consultByZipCode}/${zipCode}/json
         `);
