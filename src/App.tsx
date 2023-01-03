2d03f5bfc286b4f0518b0adedb8a7b7f70f28f6e
Aléxia Vitória <aleeevitoria@gmail.com>
2023-01-03 16:42:40 -0300

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,96 @@ import "../src/styles/global.css";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import api from "../src/services/api";
-import { useContext } from "react";
-import InputContext from "./context/inputContext";
-
+import { useState } from "react";
 
 export const App = () => {
-  const { zipCode }: any = useContext(InputContext);
-  // console.log(zipCode);
+  const [cep, setCep] = useState<string>("");
+  const [endereco, setEndereco] = useState({});
 
-  const getByZipCode = () => {
-    console.log("getByZipCode", zipCode);
-
-    api.getZipCode(zipCode)
+  const obterEnderecoCep = (value: string) => {
+    api.getZipCode(value)
       .then((response) => {
         if (response.status === 200) {
-          // exibir os dados do endereço
-          // onClick(response.data);
-          console.log(response.data);
-
+          setEndereco(response.data);
+        }
+        else if (response.status === 404) {
+          alert("CEP não encontrado!");
+          setCep("");
         }
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+    // .finally(() => { setCep(""); });
   };
+
+  function validaCEP(value: string) {
+    let RegExp = /^[0-9]{5}[0-9]{3}$/;
+
+    if (value.length > 0) {
+      if (RegExp.test(value)) {
+        obterEnderecoCep(value);
+      } else {
+        alert("CEP INVÁLIDO");
+        setCep("");
+      }
+    } else {
+      alert("Por favor digite seu CEP!");
+    }
+  }
 
   return (
     <main className="w-100 d-flex align-items-center">
       <form className="w-100 d-flex flex-column">
         <section className="d-flex w-100 align-items-end justify-content-between">
-          <Input />
-          <Button name={"consultar"} click={async () => getByZipCode()} />
+          <Input
+            name={"cep"}
+            cep={async (value) => setCep(value)}
+            isReadonly={false}
+            placeholder={"xxxxx-xxx"}
+            value={cep}
+          />
+
+          <Button
+            name={"consultar"}
+            click={async () => { validaCEP(cep) }}
+          />
         </section>
         <hr className="w-100" />
+        <section className="d-flex w-100">
+          <div className="w-100 pr-5">
+            <Input
+              name={"Logradouro"}
+              cep={async () => { }}
+              isReadonly={true}
+              placeholder={"Ex: R. Av. Trav."}
+              value={endereco.logradouro}
+            />
+
+            <Input
+              name={"Bairro"}
+              cep={async () => { }}
+              isReadonly={true}
+              placeholder={"Bairro"}
+              value={endereco.bairro}
+            />
+          </div>
+
+          <div className="w-100 pl-5">
+            <Input
+              name={"Localidade"}
+              cep={async () => { }}
+              isReadonly={true}
+              placeholder={"Localidade"}
+              value={endereco.localidade}
+            />
+            <Input
+              name={"uf"}
+              cep={async () => { }}
+              isReadonly={true}
+              placeholder={"UF"}
+              value={endereco.uf}
+            />
+          </div>
+        </section>
       </form>
     </main>
 
